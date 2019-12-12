3a6b05fd4808856422c55ff5b26ec149014cc5be
TheNFMan <nattyfelleke@gmail.com>
2019-12-11 23:26:10 -0800

    Socket on Server

--- server/app.js
+++ server/app.js
@@ -40,9 +40,7 @@ io.on('connection', function (socket) {
             initSocket(socket);
         }
     });
-    socket.on("wantedpH", function (data) {
-        arduinoSocket.emit("wantedpH", data);
-    });
+   
     socket.on("controlUpdate", function (data) {
         console.log("h");
         if(data){
@@ -55,6 +53,63 @@ io.on('connection', function (socket) {
         }
     
     });
+    socket.on("WantedpH",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedpH",data);
+    });
+    socket.on("WantedHumidity",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedHumidity",data);
+    });
+    socket.on("WantedPumpInterval",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedPumpInterval",data);
+    });
+    socket.on("WaterDrainTime",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WaterDrainTime",data);
+    });
+    socket.on("DistanceSensorHeight",function(data){
+         //TODO filter data
+        arduinoSocket.emit("DistanceSensorHeight",data);
+    });
+    socket.on("WantedpHInterval",function(data){'
+    //TODO filter data'
+        arduinoSocket.emit("WantedpHInterval",data);
+    });
+    socket.on("WantedWaterTemperatureInterval",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedWaterTemperatureInterval",data);
+    });
+    socket.on("WantedAirTemperatureInterval",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedAirTemperatureInterval",data);
+    });
+    socket.on("WantedTDSInterval",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedTDSInterval",data);
+    });
+    socket.on("WantedTurbidityInterval",function(data){
+         //TODO filter data
+        arduinoSocket.emit("WantedTurbidityInterval",data);
+    });
+    socket.on("CalibrateLowerpH",function(data){
+         //TODO filter data
+        arduinoSocket.emit("CalibrateLowerpH",data);
+    });
+    socket.on("CalibrateMiddlepH",function(data){
+         //TODO filter data
+        arduinoSocket.emit("CalibrateMiddlepH",data);
+    });
+    socket.on("CalibrateUpperpH",function(data){
+         //TODO filter data
+        arduinoSocket.emit("CalibrateUpperpH",data);
+    });
+    socket.on("CalibrateTDS",function(data){
+        //TODO filter data
+        arduinoSocket.emit("CalibrateTDS",data);
+    });
+   
 })
 
 http.listen(3000, function () {
